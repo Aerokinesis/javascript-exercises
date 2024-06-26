966fc69173da1b41707c0c9275ccf17d4e873da2
Aerokinesis <alexwduong@gmail.com>
2024-06-25 23:24:24 -1000

    Completed exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let finalSum = 0;
 
+
+    
+    if (typeof start != "number" || typeof end != "number"){
+        return "ERROR";
+    }
+
+    if (start < 0 || end < 0) {
+        return "ERROR";
+    }
+    if (start > end) {
+        let temp;
+
+        temp = end;
+        end = start;
+        start = temp;
+    }
+
+    for (let i = start; i <= end; i++) {
+        finalSum += i;
+    }
+
+    return finalSum;
 };
 
 // Do not edit below this line
