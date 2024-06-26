12e4b09f73d725167233342ca6a0ed9eeedc6983
Aerokinesis <alexwduong@gmail.com>
2024-06-25 22:00:42 -1000

    Completed 2nd exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,24 @@
-const repeatString = function() {
+const repeatString = function(str, repeatAmount = randInt()) {
+    function randInt() {
+        return Math.floor(Math.random() * 1000);
+    }
+    
+    let word = "";
+    if (str == "") {
+        return "";
+    }
+    if (repeatAmount < 0) {
+        return "ERROR";
+    }
+    if (repeatAmount == 0) {
+        return word;
+    }
 
+    for (let i = 0; i < repeatAmount; i++) {
+        word += str;
+    }
+
+    return word;
 };
 
 // Do not edit below this line
