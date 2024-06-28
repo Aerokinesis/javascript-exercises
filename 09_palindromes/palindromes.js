c3c506bf082e30d00d8558df8489cb593e210280
Aerokinesis <alexwduong@gmail.com>
2024-06-27 21:27:37 -1000

    Completed exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let cleanStr = str.replace(/\W/g, '').toLowerCase();
+    
+    let reverseStr = cleanStr.split("").reverse().join("").toLowerCase();
 
+    if (reverseStr == str || cleanStr == reverseStr) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
