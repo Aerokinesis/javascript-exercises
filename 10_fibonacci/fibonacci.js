c38a3215c23ab42ee53f7c7f7ef38a944f82255e
Aerokinesis <alexwduong@gmail.com>
2024-07-06 07:15:01 -1000

    Removed rps and it's branch. completed exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let stringToNum = 0;
 
-};
+    if (typeof num !== Number) {
+        stringToNum = parseInt(num);
+    } else {
+        stringToNum = num;
+    }
+
+    if (stringToNum < 0) return "OOPS";
+    if (stringToNum == 0) return 0;
+    if (stringToNum == 1) return 1;
+        
+    return fibonacci(stringToNum - 2) + fibonacci(stringToNum - 1);
+    };
 
 // Do not edit below this line
 module.exports = fibonacci;
